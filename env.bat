bc799cfbc6920750b0beb38e1b91a7ebcf1e1fc2
s-watanabe13 <57216254+s-watanabe13@users.noreply.github.com>
2024-07-17 12:14:23 +0900

    hoge

--- env.bat
+++
@@ -1,6 +0,0 @@
-py -m venv .venv
-cd .venv/Scripts
-call "activate.bat"
-py -m pip install --upgrade pip
-cd ../..
-pip install -r requirements.txt
--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 setup(
     name="measurement_codes_ut",
     packages=find_packages(),
-    version="1.0.8",
+    version="0.0.0",
     install_requires = [
         # Github Private Repository
         'plottr@git+https://github.com/qipe-nlab/plottr.git@search-datadict',
